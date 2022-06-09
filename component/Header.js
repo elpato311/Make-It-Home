6648e840eb206d3804c991c8377cb07d2fe35b37
felzhraa <elpato311>
2022-06-09 02:57:43 +0200

    feat: add logo

--- component/Header.js
+++ component/Header.js
@@ -5,23 +5,21 @@ import tw from 'twrnc';
 
 export default function Header() {
   return (
-    <View style={tw`flex-row pt-4 pl-12`}>
-      <Image style={tw`w-8 h-8 mt-.5 `} source={require('./assets/logo.png')} />
-      <Text style={tw`pr-4 font-serif text-2xl font-bold text-violet-800`}>
-        Artist
-      </Text>
+    <View style={tw`flex-row flex-wrap pt-4 pl-8`}>
+      <Image style={tw`w-23 h-9 mt-.5  pl-8 ml-4`} resizeMode= '' source={require('./assets/logs.png')} />
+     
       <TextInput
         style={tw`pl-2 rounded shadow-lg w-90`}
         placeholder="Search for products.."
       />
-      <View style={tw`w-10 pl-2 rounded shadow-lg bg-violet-600`}>
+      <View style={tw`w-10 pl-2 rounded shadow-lg bg-cyan-300			`}>
         <FontAwesome style={tw`pt-1`} name="search" size={22} />
       </View>
-      <View style={tw`w-10 pl-2 bg-white shadow-lg rounded-xl ml-200`}>
+      <View style={tw`w-10 pl-2 bg-cyan-300		 shadow-lg rounded-xl ml-180`}>
         <FontAwesome style={tw`pt-1 `} name="shopping-cart" size={22} />
       </View>
       <View
-        style={tw`p-2 ml-2 text-white rounded-lg shadow-lg w-18 bg-violet-600 `}
+        style={tw`p-2 ml-2 text-white rounded-lg shadow-lg w-18 bg-cyan-300		`}
       >
         <Text style={tw`text-white font-lg `}>Sign Up</Text>
       </View>
