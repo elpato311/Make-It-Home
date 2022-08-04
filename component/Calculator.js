aebc8bd0e28710ffe0e7a600cd0d0e41fbe3c4e6
felzhraa <elpato311>
2022-08-04 03:36:59 +0200

    feat: add state for Picker

--- component/Calculator.js
+++ component/Calculator.js
@@ -6,32 +6,32 @@ import { useState } from "react";
 
 
 export default function Calculator() {
-  const[height, SetHeight] = useState('')
-  const[width, setWidth]= useState('')
-  const[Walls, setWall]= useState('')
-  const[products, SetProducts]= useState('')
+  const [height, SetHeight] = useState('')
+  const [width, setWidth] = useState('')
+  const [Walls, setWall] = useState('')
+  const [wallType, setWallType] = useState('')
+  const [products, SetProducts] = useState('')
   return (
-    <View style={tw`  shadow-lg   border-2 border-gray-300 m-10 px-3 py-22  items-center bg-white   `}>
+    <View style={tw`shadow-lg border-2 border-gray-300 m-10 px-3 py-22 items-center bg-white`}>
       <View style={tw`w-full md:w-1/3`}>
 
-        <Text style={tw`pb-2 `}>Wall Type</Text>
-        <Picker style={tw`h-10 border-gray-300	  `}
-
-          onValueChange={(value) => console.log(value)}
+        <Text style={tw`pb-2`}>Wall Type</Text>
+      
+        <Picker style={tw`h-10 border-gray-300	`}
+          selectedValue={wallType} onValueChange={value => setWallType(value)}
         >
-
           <Picker.Item label="Exterior" value="Exterior" />
           <Picker.Item label="Interior" value="Interior" />
 
 
         </Picker >
 
-        <Text style={tw`pb-2 `}>Products</Text>
+        <Text style={tw`pb-2`}>Products</Text>
 
-        <Picker style={tw` h-10  border-gray-300 	 ` }
-
-          onValueChange={(value) => console.log(value)}
+        <Picker style={tw`h-10 border-gray-300`}
+          selectedValue={products} onValueChange={value => SetProducts(value)}
         >
+
 
           <Picker.Item label="Couch Primer" value="Couch Primer" />
           <Picker.Item label="Geltex-20000" value="Geltex-20000" />
@@ -42,27 +42,29 @@ export default function Calculator() {
 
         <View style={tw`flex-row flex-wrap justify-between mt-2`}>
           <View style={tw`w-full md:w-48%`}>
-            <Text style={tw` mb-1`}>Height</Text>
-            <TextInput style={tw`h-8 border border-gray-300	 bg-white`} placeholder='' value= {height} onChangeText={num => SetHeight(num)} />
+            <Text style={tw`mb-1`}>Height</Text>
+            <TextInput style={tw`h-8 border border-gray-300	 bg-white`} placeholder='' value={height} onChangeText={num => SetHeight(num)} />
           </View>
           <View style={tw`w-full md:w-48%`}>
-            <Text style={tw` mb-1`}>Width</Text>
-            <TextInput style={tw`h-8 border border-gray-300	bg-white`} placeholder='' value= {width} onChangeText={meters => setWidth(meters)} />
+            <Text style={tw`mb-1`}>Width</Text>
+            <TextInput style={tw`h-8 border border-gray-300	bg-white`} placeholder='' value={width} onChangeText={meters => setWidth(meters)} />
           </View>
         </View>
-        <View style={tw` justify-center`}>
-          <Text style={tw` mb-1`}>Number of Walls</Text>
-          <TextInput style={tw`h-8 border  border-gray-300 bg-white  `}  placeholder='' value= {Walls} onChangeText={number => setWall(number)}  />
+        <View style={tw`justify-center`}>
+          <Text style={tw`mb-1`}>Number of Walls</Text>
+          <TextInput style={tw`h-8 border border-gray-300 bg-white`} placeholder='' value={Walls} onChangeText={number => setWall(number)} />
         </View>
-        <View style={tw` mt-4 flex-row w-full justify-between `} >
-          <Pressable style={tw`w-49% h-8 border items-center justify-center bg-red-400 border-gray-300`}>
+        <View style={tw`mt-4 flex-row w-full justify-between`} >
+          <Pressable 
+          style={tw`w-49% h-8 border items-center justify-center bg-red-400 border-gray-300`}
+          >
             <Text style={tw`text-white`}>Calculate</Text>
           </Pressable>
-          
-          <Text style={tw`w-49% h-8 border  bg-stone-50  border-gray-300	text-center `} >Clear</Text>
+
+          <Text style={tw`w-49% h-8 border bg-stone-50 border-gray-300	text-center`} >Clear</Text>
 
         </View>
-        <Text>SEE HOW MUCH PAINT YOU'LL NEED You need {} liters</Text>
+        <Text>SEE HOW MUCH PAINT YOU'LL NEED You need { } liters</Text>
 
       </View>
     </View>)
